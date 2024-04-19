f26c6616c22bb842bfb83502d657418f56390072
Marcelo Lopes <marcelo.cae@gmail.com>
2024-04-18 21:55:38 -0300

    Ajuste de select errado

--- app_super_gestao/resources/views/app/produto/_components/form_create_edit.blade.php
+++ app_super_gestao/resources/views/app/produto/_components/form_create_edit.blade.php
@@ -9,7 +9,7 @@
         <select name="fornecedor_id" class="borda-preta">
             <option value="">Selecionar o Fornecedor</option>
             @foreach ($fornecedores as $fornecedor)
-            <option value="{{ $fornecedor->id }}" {{ $fornecedor->id ?? old('fornecedor_id') == $fornecedor->id || ($fornecedor->id == $fornecedor_id) ? 'selected' : '' }}>{{ $fornecedor->nome }}</option>
+            <option value="{{ $fornecedor->id }}" {{ ($produto->fornecedor_id ?? old('fornecedor_id')) == $fornecedor->id ? 'selected' : '' }} >{{ $fornecedor->nome }}</option>
             @endforeach
         </select>
         {{ $errors->has('fornecedor_id') ? $errors->first('fornecedor_id') : '' }}
